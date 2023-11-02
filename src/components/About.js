818cdffe550f85d30716c0fdc79a01ee0d7eb8e5
Kinsey Neas <kneas@trinity.edu>
2023-11-02 01:30:27 -0500

    updated bio

--- src/components/About.js
+++ src/components/About.js
@@ -6,10 +6,12 @@ const About = () => {
     <>
         <div className="md:flex py-16 px-2 items-center">
             <div className="portrait w-1/2 h-auto pr-8 grid place-items-center"><img className="justify-center items-center" src={portrait}/></div>   
-            <div>
+            <div className="md:w-3/4">
                 <h1 className="text-4xl font-semibold py-2">Kinsey Neas</h1>
                 <h3 className="text-2xl italic py-1">Creator and Developer</h3>
-                <p className="text-md lg:text-lg py-3 text-gray-600">Hello! My name is Kinsey Neas and I love to tell stories! Feel free to look at my work below!</p>
+                <p className="text-md lg:text-lg py-3 text-gray-600">
+                  Hello! My name is Kinsey Neas and I am student passionate telling stories and solving problems through art and code. Currently pursuing a BS in Computer Science and minor in Art at Trinity University, I’m on a mission to shape the digital world through imaginative software solutions and artistic innovation. Whether I am coding, creating, or throwing a frisbee, I’m always eager to explore new opportunities and tackle challenges with an open, curious, and collaborative mindset! Please feel free to view my creative work below!
+                </p>
             </div>
         </div>    
     </>
